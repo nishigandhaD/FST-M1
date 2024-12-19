0d3f6f932108931b0b942b09f1563491229d5e84
nishigandhaD <Nishigandha.Darure@ibm.com>
2024-12-19 12:21:27 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,13 @@
+package Activites;
+
+public class Activity1 {
+	public static void main(String[] args) {
+		Car swift=new Car();// TODO Auto-generated method stub
+		swift.color="Black";
+		swift.make=2014;
+		swift.transmission="manual";
+		swift.displayCharacteristics();
+		swift.accelarate();
+		swift.brake();
+	}
+}
