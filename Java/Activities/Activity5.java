f2e1a59865a930d63262fbf0a6dd053b60f2a115
nishigandhaD <Nishigandha.Darure@ibm.com>
2025-07-24 13:06:13 +0530

    Add files via upload

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,17 @@
+
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		String title = "Hover Car Racer";
+        
+        Book5 newNovel = new MyBook5();
+        
+        newNovel.setTitle(title);
+        
+        
+        System.out.println("The title is: " + newNovel.getTitle());// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java/Activities/MyBook5.java
@@ -0,0 +1,8 @@
+
+
+public class MyBook5 extends Book5{
+	 public void setTitle(String s) {
+	        title = s;
+	    }
+
+}
